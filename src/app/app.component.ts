a4243ade71b6baea4725172aa852e421dcbe5ecd
genildocs <genildocs@gmail.com>
2024-12-21 06:19:00 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ServiceService } from './data/service.service';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'product-list-angular';
+export class AppComponent implements OnInit {
+  data: any;
+  constructor(private service: ServiceService) {}
+
+  ngOnInit(): void {
+    this.service.getData().subscribe((response) => {
+      this.data = response;
+    });
+  }
 }
